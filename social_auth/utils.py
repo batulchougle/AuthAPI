25c6e88301d2582932503d9e7f669b56d8b8e008
batulchougle <batulchougle@gmail.com>
2025-09-26 13:24:36 +0530

    Add files via upload

---
+++ social_auth/utils.py
@@ -0,0 +1,59 @@
+from google.auth.transport import requests
+from google.oauth2 import id_token
+from account.models import User
+from django.contrib.auth import authenticate
+from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
+
+class Google():
+    @staticmethod
+    def validate(access_token):
+        try:
+            id_info=id_token.verify_oauth2_token(access_token, requests.Request())
+            if "accounts.google.com" in id_info['iss']:
+                return id_info
+        except:
+            return "token is invalid or expired"  
+
+def login_social_user(email,password):
+    user=authenticate(email=email,password=password)
+    user_tokens=user.tokens()
+    return {
+    'email':user.email,
+     'name': user.get_name,
+     'username': user.get_username,
+     'access_token':str(user_tokens.get('access')),
+     'refresh_token':str(user_tokens.get('refresh'))
+    }     
+
+def register_social_user(provider,email,name):
+    user=User.objects.filter(email=email)
+    if user.exists():
+        if provider== user[0].auth_provider:
+            login_social_user(email,settings.SOCIAL_AUTH_PASSWORD)
+            
+        else:
+           raise AuthenticationFailed(
+           detail=f"please continue your login with{user[0].auth_provider}"
+       ) 
+    else:
+          new_user={
+              'email':'email',
+              'username':'username',
+              'name':'name',
+              'password':settings.SOCIAL_AUTH_PASSWORD
+          }  
+          register_user= User.objects.create_user(**new_user)
+          register_user.auth_provider=provider
+          register_user.is_verified=True
+          register_user.save()
+          login_social_user(email=register_user.email, password=settings.SOCIAL_AUTH_PASSWORD)
+                                                  
+          
+          
+          
+
+
+
+
+
